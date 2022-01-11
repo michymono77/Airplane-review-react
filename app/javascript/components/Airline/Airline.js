d541f5f00293a7576bbe71cf97de2fed6e9db9a4
Michiharu Ono <michiharuono77@gmail.com>
2022-01-11 19:01:53 +0900

    Loading airline data from api on Header.js

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,22 +1,42 @@
 import React, {useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from "react-router-dom"
+import Header from './Header'
+import styled from 'styled-components'
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({})
   const [review, setReview] = useState({})
   const params = useParams();
+  const [loaded, setLoaded] = useState(false)
 
 
   useEffect(()=>{
     const url = `/api/v1/airlines/${params.slug}.json`
     axios.get(url)
-      .then( resp => console.log(resp) )
+      .then( resp => {
+        setAirline(resp.data)
+        setLoaded(true)
+      })
       .catch( resp => console.log(resp) )
   }, [])
 
   return (
-  <div>This is the Airlines#show view for the app</div>
+  <div className="wrapper">
+    <div className="column">
+      {
+        loaded &&
+        <Header
+          attributes={airline.data.attributes}
+          reviews={airline.included}
+        />
+      }
+        <div className="reviews"></div>
+    </div>
+    <div className="column">
+        <div className="review-form">[Review form goes here]</div>
+    </div>
+  </div>
   )
 }
 
