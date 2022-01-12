3229ecaae5a4b326043f9ea245c735ba210a32ce
Michiharu Ono <michiharuono77@gmail.com>
2022-01-12 21:13:13 +0900

    svg encoding

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,6 +1,59 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import styled from 'styled-components'
+import Gray from './Stars/Gray'
+import Hover from './Stars/Hover'
+import Selected from './Stars/Selected'
+
+const RatingContainer = styled.div`
+  text-align: center;
+  border-radius: 4px;
+  font-size: 16px;
+  padding: 40px 0 10px 0;
+  border: 1px solid #e6e6e6;
+  background: #fff;
+`
+const RatingBox = styled.div`
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  flex-direction: row-reverse;
+  position: relative;
+
+  input {
+    display: none;
+  }
+
+  label {
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    background-image: url("data:image/svg+xml,${Gray}");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 70%;
+  }
+
+  input:checked ~ label,
+  input:checked ~ label ~ label {
+    background-image: url("data:image/svg+xml,${Selected}");
+  }
+
+    input:not(:checked) ~ label:hover,
+  input:not(:checked) ~ label:hover ~ label {
+    background-image: url("data:image/svg+xml,${Hover}");
+  }
+`
+const RatingTitle = styled.div``
 
 const ReviewForm = (props) => {
+  const ratingOptions = [5,4,3,2,1].map ( (score, index) => {
+    return (
+    <Fragment>
+      <input type="radio" value={score} name="rating" onChange={() => console.log('selected:', score)} id={`rating-${score}`} />
+      <label></label>
+    </Fragment>
+    )
+  })
   return(
     <div className="wrapper">
 
@@ -13,11 +66,12 @@ const ReviewForm = (props) => {
           <input onChange={props.handleChange} value={props.review.description} type="text" name="description" placeholder="Review Description" />
         </div>
         <div className="field">
-          <div className="rating-container">
-
+          <RatingContainer>
             <div className="rating-title-text">Rate This Airline</div>
-            [Star Rating Goes Here]
-          </div>
+            <RatingBox>
+              {ratingOptions}
+            </RatingBox>
+          </RatingContainer>
         </div>
         <button type="submit">Submit Your Review</button>
       </form>
