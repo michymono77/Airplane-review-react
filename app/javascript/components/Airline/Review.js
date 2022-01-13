c172d5c2d5eefbab77bae7773a11f8d07e488581
Michiharu Ono <michiharuono77@gmail.com>
2022-01-13 14:06:41 +0900

    Fontawesome implemented

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import Rating from '../Rating/Rating'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid rgba(0,0,0,0.1);
+  border-radius: 4px;
+  padding: 16px;
+  margin: 0 16px 16px 0;
+`
+const RatingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Title = styled.div`
+  padding: 16px 0 0 0;
+  font-size: 24px;
+`
+const Description = styled.div`
+  padding: 0 0 16px 0;
+  font-size: 16px;
+`
 
 const Review = (props) => {
   const { score, title, description } = props.attributes
   return (
-    <div className="card">
-      <div className="rating-container">
-        <div className="rating-score">{score}</div>
-      </div>
-      <div className="title">{title}</div>
-      <div className="description">{description}</div>
-    </div>
+    <Card>
+      <RatingContainer>
+        <Rating score={score} />
+      </RatingContainer>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Card>
   )
 }
 
